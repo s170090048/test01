d733d03fa34945be260caafa4c8b7834675b0b9c
jason <249116620@qq.com>
2018-12-02 17:47:34 +0800

    first Eidtion

--- src/testMain.java
+++ src/testMain.java
@@ -2,5 +2,6 @@ public class testMain {
     public static void main(String[] args) {
         System.out.println("hehe");
         System.out.println("why R U so stupid");
+        System.out.println("haha");
     }
 }
